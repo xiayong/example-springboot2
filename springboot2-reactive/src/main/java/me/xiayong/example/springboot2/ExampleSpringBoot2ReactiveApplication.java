e704ead73bef485773caa11bcf45a6424ef56962
Andrew <i.xiayong@me.com>
2021-08-11 11:15:48 +0800

    项目调整

--- springboot2-reactive/src/main/java/me/xiayong/example/springboot2/ExampleSpringBoot2ReactiveApplication.java
+++ springboot2-reactive/src/main/java/me/xiayong/example/springboot2/ExampleSpringBoot2ReactiveApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ExampleSpringBoot2Application {
+public class ExampleSpringBoot2ReactiveApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ExampleSpringBoot2Application.class, args);
+		SpringApplication.run(ExampleSpringBoot2ReactiveApplication.class, args);
 	}
 }
